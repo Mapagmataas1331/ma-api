2c21e448e732249a2a384ad747c12f9b5a8c96ab
Mapagmataas <98333916+Mapagmataas1331@users.noreply.github.com>
2024-11-17 22:25:13 +0700

    Added og button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,10 @@
 <div
 	class="mx-auto flex min-h-[calc(100dvh_-_3.5rem)] w-full flex-col items-center justify-center bg-gradient-to-br from-white to-sky-200 px-1 dark:from-sky-800 dark:to-black md:min-h-[calc(100dvh_-_4rem)]"
 >
-	<Button variant="outline" href="https://ma.cyou" class="mb-4"
+	<Button variant="outline" href="https://ma.cyou/" class="mb-4"
 		>{$language === 'ru' ? 'Основной сайт автора' : "Author's main site"}</Button
+	>
+	<Button variant="outline" href="https://test.ma.cyou/og/"
+		>{$language === 'ru' ? 'Создать OG картинку' : 'Create OG image'}</Button
 	>
 </div>
