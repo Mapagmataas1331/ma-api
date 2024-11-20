8b0415c87220f85862f0e66d2761f19b7ecc4343
Mapagmataas <98333916+Mapagmataas1331@users.noreply.github.com>
2024-11-21 01:28:04 +0700

    Minor improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
 	import Header from '$shared/components/Header.svelte';
 	import { Toaster } from '$shared/components/ui/sonner/index.js';
+	import Footer from '$shared/components/Footer.svelte';
 	import '@/app.scss';
 
 	let { children } = $props();
 </script>
 
-<Header profile={false} />
+<Header
+	links={{
+		'https://ma.cyou/': { name: 'Home', nameRu: 'Главная' },
+		'/': { name: 'api', nameRu: 'api' }
+	}}
+/>
 
 <Toaster />
 
 {@render children?.()}
+
+<Footer
+	links={{
+		'https://ma.cyou/': { name: "Author's site", nameRu: 'Сайт Автора' },
+		'https://github.com/mapagmataas1331/ma-api/': { name: 'Source code', nameRu: 'Исходный код' }
+	}}
+/>
